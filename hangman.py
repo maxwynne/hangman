196cb0cfd62d0f21a16504ff4b5a06e0b1bf2f79
maxwynne <76392789+maxwynne@users.noreply.github.com>
2022-01-05 20:15:40 +0000

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,64 +1,82 @@
 import random
+
 from words import word_list
 
-def getRandomWord(word_list):
-    wordIndex = random.randint(0, len(wordList) - 1)
-    return word_list[wordIndex].upper()
+def get_word():
+    word = random.choice(word_list)
+    # return random word in all uppercase, convert all user input to uppercase to make comparison logic simpler
+    return word.upper()
 
+# create variables updating for each turn user takes
+# display word for each turn
 def play(word):
-    word_complete = "_" * len(word)
+    word_completion = "_" * len(word)
+    # guessed variable initialised to false
     guessed = False
     guessed_letters = []
     guessed_words = []
+    # bodyparts left to be drawn
     tries = 6
     print("Let's play Hangman!")
-    print(display_hangman(tries))
+    # print initial stage of hangman
+    print(display-hangman(tries))
+    # initial stage of word with all underscores
     print(word_completion)
     print("\n")
-    
+    # while loop to get tries
     while not guessed and tries > 0:
         guess = input("Please guess a letter or word: ").upper()
+        # guess letter, word or typing other than single letter or word of correct length
         if len(guess) == 1 and guess.isalpha():
+            # conditional block to see if letter already guessed, not in word or is in words
             if guess in guessed_letters:
-                print("You already guessed the letter", guess)
+                print("You already guessed the letter", guess) #guess is print the letter
             elif guess not in word:
                 print(guess, "is not in the word.")
                 tries -= 1
                 guessed_letters.append(guess)
             else:
-                print("good job,", guess "is in the word!")
+                print("Good job," guess, "is in the word!")
                 guessed_letters.append(guess)
+                # update word completion for all occurences of guess
+                # convert word completion from string to list to index into it, store in new variable: word_as_list
                 word_as_list = list(word_completion)
-
-        elif len(guess) == len(word) and guess.isalpha()
+                indices = [i for i, letter in enumerate(word) if letter == guess)
+                for index in indices:
+                    word_as_list[index] = guess
+                # index guess letters into word
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
-                print("You already guessed the word", guess)
+                print("You already guessed the word!," guess)
             elif guess != word:
-                print(guess, "is not in the word.")
+                print(guess, "is not the word")
                 tries -= 1
-                guessed_words.append(guess)
+                guessed_word.append(guess)
             else:
                 guessed = True
                 word_completion = word
 
         else:
-            print("Not a valid guess")
+            print("Not a valid guess.")
+        # print new line to space out each term
         print(display_hangman(tries))
         print(word_completion)
         print("\n")
     if guessed:
-        print("Congratulations, you guessed the word!")
+        print("You guessed the word!")
     else:
-        print("You ran out of words. You have failed.")
+        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
 
-# get word from get_word
 def main():
     word = get_word()
     play(word)
-    while input("Play Again? (Y/N) ").upper() == "Y":
+    while input("Play again (Y/N) ").upper() == "Y":
         word = get_word()
         play(word)
 
-
+# make sure it runs on the command line
 if __name__ == "__main__":
     main()
